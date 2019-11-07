16135b4cc5aa62e9137e69e9333656b362eb24f9
filippo <schianchi.filippo.93@gmail.com>
2019-11-07 17:12:28 +0100

    fatto esercizio base

---
+++ javascript/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+   
+    $(".next").click(function() {
+        var posizioneImg = $(".slider-wrapper .images img.active");
+        var posizionePall = $(".nav i.active")
+        posizioneImg.removeClass("active");
+        posizionePall.removeClass("active");
+        if(posizioneImg.hasClass("last")) {
+           $(".slider-wrapper .images img.first").addClass("active");
+           $(".nav i.first").addClass("active");
+        } else { 
+            posizioneImg.next("img").addClass("active");
+            posizionePall.next("i").addClass("active");
+        }
+    });
+
+    $(".prev").click(function() { 
+        var posizioneImg = $(".slider-wrapper .images img.active");
+        var posizionePall = $(".nav i.active")
+        posizioneImg.removeClass("active");
+        posizionePall.removeClass("active");
+        if(posizioneImg.hasClass("first")) {
+           $(".slider-wrapper .images img.last").addClass("active");
+           $(".nav i.last").addClass("active");
+        } else { 
+            posizioneImg.prev("img").addClass("active");
+            posizionePall.prev("i").addClass("active");
+
+        }
+    });
+
+
+
+
+
+});
+    
